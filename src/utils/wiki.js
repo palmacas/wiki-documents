14559bb5f606c2228fcd1887fa6880b6709dd06a
Jessie219-web <112080869+Jessie219-web@users.noreply.github.com>
2024-10-30 16:52:39 +0800

    update t1000-e version comparison

--- src/utils/wiki.js
+++ src/utils/wiki.js
@@ -1,65 +1,65 @@
 const docList = [
   [
     {
+      "path": "/sensecap_indicator_meshtastic",
+      "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
+      "title": "Get Started with Indicator for Meshtastic",
+      "date": "10/29/2024"
+    },
+    {
       "path": "/recomputer_r1000_thingsboard_dashboard",
       "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/recomputer_r_images/01.png",
       "title": "Create Dynamic IoT Dashboards with ThingsBoard and reComputer R1000",
       "date": "10/25/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/recomputer_r1000_flash_OS",
       "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
       "title": "reComputer R1000 Raspbian OS to eMMC",
       "date": "10/25/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/reComputer_r1000_use_rs485_modbus_rtu",
       "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
       "title": "How to use rs485 and modbus with reComputer R1000",
       "date": "10/25/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/reComputer_r1000_use_bacnet_mstp",
       "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
       "title": "reComputer R1000 use bacnet MS/TP",
       "date": "10/25/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/wio_sx1262_xiao_esp32s3_for_lora_sensor_node",
       "image": "https://files.seeedstudio.com/wiki/XIAO_ESP32S3_for_Meshtastic_LoRa/2.png",
       "title": "Configuring Wio-SX1262 with XIAO ESP32S3 kit as LoRaWAN Sensor Node",
       "date": "10/23/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/getting_started_with_watcher_task",
       "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/85.png",
       "title": "SenseCAP Watcher Task Assignment Guideline",
       "date": "10/18/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/watcher_hardware_overview",
       "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/hardware_overview.jpg",
       "title": "SenseCAP Watcher Hardware Overview",
       "date": "10/18/2024"
-    },
-    {
-      "path": "/watcher_operation_guideline",
-      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/0.JPG",
-      "title": "SenseCAP Watcher Operation Guideline",
-      "date": "10/18/2024"
     }
   ],
   [
     {
-      "path": "/watcher_price",
-      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/price_month_simpler.png",
-      "title": "SenseCraft AI for Watcher Plans and Benefits",
+      "path": "/watcher_operation_guideline",
+      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/0.JPG",
+      "title": "SenseCAP Watcher Operation Guideline",
       "date": "10/18/2024"
     },
     {
@@ -77,10 +77,10 @@ const docList = [
       "date": "10/18/2024"
     },
     {
-      "path": "/wio_sx1262_xiao_esp32s3_LNS_Chirpstack",
-      "image": "https://files.seeedstudio.com/wiki/XIAO_ESP32S3_for_Meshtastic_LoRa/2.png",
-      "title": "Wio-SX1262 with XIAO ESP32S3 Kit Connect to Chirpstack",
-      "date": "10/17/2024"
+      "path": "/watcher_price",
+      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/price_month_simpler.png",
+      "title": "SenseCraft AI for Watcher Plans and Benefits",
+      "date": "10/18/2024"
     }
   ]
 ];
