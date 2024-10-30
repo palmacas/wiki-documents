af69e41b9249cc1b15cfbd60a3a736e852abb183
Jessie219-web <112080869+Jessie219-web@users.noreply.github.com>
2024-10-30 09:53:26 +0800

    Revert changes to src/utils/wiki.js

--- src/utils/wiki.js
+++ src/utils/wiki.js
@@ -1,79 +1,79 @@
 const docList = [
   [
     {
-      "path": "/sensecap_indicator_meshtastic",
-      "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
-      "title": "Get Started with Indicator for Meshtastic",
-      "date": "10/29/2024"
-    },
-    {
       "path": "/weekly_wiki",
       "image": "https://files.seeedstudio.com/wiki/IndexWiki/logo_image.jpg",
       "title": "Weekly Wiki",
       "date": "10/28/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/recomputer_r1000_thingsboard_dashboard",
       "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/recomputer_r_images/01.png",
       "title": "Create Dynamic IoT Dashboards with ThingsBoard and reComputer R1000",
       "date": "10/25/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/recomputer_r1000_flash_OS",
       "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
       "title": "reComputer R1000 Flash OS",
       "date": "10/25/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/reComputer_r1000_use_rs485_modbus_rtu",
       "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
       "title": "How to use rs485 and modbus with reComputer R1000",
       "date": "10/25/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/reComputer_r1000_use_bacnet_mstp",
       "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
       "title": "reComputer R1000 use bacnet MS/TP",
       "date": "10/25/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/wio_sx1262_xiao_esp32s3_for_lora_sensor_node",
       "image": "https://files.seeedstudio.com/wiki/XIAO_ESP32S3_for_Meshtastic_LoRa/2.png",
       "title": "LoRaWAN Sensor Node",
       "date": "10/23/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "",
       "image": "https://files.seeedstudio.com/wiki/IndexWiki/logo_image.jpg",
       "title": "Weekly Wiki",
       "date": "10/21/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/getting_started_with_watcher_task",
       "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/85.png",
       "title": "Task Assignment Guideline",
       "date": "10/18/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/watcher_hardware_overview",
       "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/hardware_overview.jpg",
       "title": "Hardware overview",
       "date": "10/18/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/watcher_operation_guideline",
       "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/0.JPG",
       "title": "Operation Guideline",
+      "date": "10/18/2024"
+    }
+  ],
+  [
+    {
+      "path": "/watcher_price",
+      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/price_month_simpler.png",
+      "title": "Price and Benefits",
       "date": "10/18/2024"
     },
     {
