5efbcd1dca60aa94edb379368b479198fc79fab1
Jessie219-web <112080869+Jessie219-web@users.noreply.github.com>
2024-11-08 14:15:39 +0800

    add t1000-e video guide

--- src/utils/wiki.js
+++ src/utils/wiki.js
@@ -1,86 +1,86 @@
 const docList = [
   [
     {
+      "path": "/xiao_mg24_matter",
+      "image": "https://files.seeedstudio.com/wiki/XIAO_MG24/Getting_Start/top.jpg",
+      "title": "Seeed Studio XIAO MG24 Example - Matter",
+      "date": "2024-11-07T06:43:35.000Z"
+    },
+    {
       "path": "/watcher_software_framework",
       "image": "https://files.seeedstudio.com/wiki/watcher_software_framework/architecture_1.webp",
       "title": "Software Architecture",
       "date": "11/5/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/watcher_function_module_development_guide",
       "image": "https://files.seeedstudio.com/wiki/watcher_software_framework/watcher_function_module.webp",
       "title": "Watcher Function Module Development Guide",
       "date": "11/5/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/build_watcher_development_environment",
       "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/64.webp",
       "title": "Build the Watcher Development Environment",
       "date": "11/5/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/watcher_ui_integration_guide",
       "image": "https://files.seeedstudio.com/wiki/watcher_software_framework/ui_framework.webp",
       "title": "UI Integration Guide",
       "date": "11/5/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/xiao_rp2350_arduino",
       "image": "https://files.seeedstudio.com/wiki/XIAO-RP2350/img/2-102010550_XIAO_RP2350-45font_1.webp",
       "title": "Seeed Studio XIAO RP2350 with Arduino",
       "date": "2024-10-30T01:39:16.136Z"
-    },
+    }
+  ],
+  [
     {
       "path": "/sensecap_indicator_meshtastic",
       "image": "https://files.seeedstudio.com/wiki/SenseCAP/Meshtastic/indicator-page_1.webp",
       "title": "Get Started with Indicator for Meshtastic",
       "date": "10/29/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/recomputer_r1000_thingsboard_dashboard",
       "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/tb/dashboard/thingsboard_1.webp",
       "title": "Create Dynamic IoT Dashboards with ThingsBoard and reComputer R1000",
       "date": "10/25/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/recomputer_r1000_flash_OS",
       "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/recomputer_r_images/01_1.webp",
       "title": "reComputer R1000 Raspbian OS to eMMC",
       "date": "10/25/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/reComputer_r1000_use_rs485_modbus_rtu",
       "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/recomputer_r_images/r1000_connection_1.webp",
       "title": "How to use rs485 and modbus with reComputer R1000",
       "date": "10/25/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/reComputer_r1000_use_bacnet_mstp",
       "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/RS485_fix/wirshark_capture.webp",
       "title": "reComputer R1000 use bacnet MS/TP",
       "date": "10/25/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/wio_sx1262_xiao_esp32s3_for_lora_sensor_node",
       "image": "https://files.seeedstudio.com/wiki/XIAO_ESP32S3_for_Meshtastic_LoRa/67_1.webp",
       "title": "Configuring Wio-SX1262 with XIAO ESP32S3 kit as LoRaWAN Sensor Node",
       "date": "10/23/2024"
-    },
-    {
-      "path": "/getting_started_with_watcher_task",
-      "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/85_1.webp",
-      "title": "SenseCAP Watcher Task Assignment Guideline",
-      "date": "10/18/2024"
     }
   ]
 ];
