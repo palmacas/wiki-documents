222e95e9a4a9db9ce2308a0566f721c3e984159e
Jessie219-web <112080869+Jessie219-web@users.noreply.github.com>
2024-11-12 17:58:13 +0800

    update S210x decoder

--- src/utils/wiki.js
+++ src/utils/wiki.js
@@ -1,34 +1,6 @@
 const docList = [
   [
     {
-      "path": "/recamera_get_start",
-      "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
-      "title": "WIP...",
-      "date": "11/11/2024"
-    },
-    {
-      "path": "/reCamera_hardware_interface",
-      "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
-      "title": "WIP...",
-      "date": "11/11/2024"
-    }
-  ],
-  [
-    {
-      "path": "/recamera_model_conversion",
-      "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
-      "title": "WIP...",
-      "date": "11/11/2024"
-    },
-    {
-      "path": "/iot_botton_connect_to_esphome",
-      "image": "https://files.seeedstudio.com/wiki/IoT_Botton_ESPHOME/BUTTON/1.png",
-      "title": "Enable logging",
-      "date": "11/11/2024"
-    }
-  ],
-  [
-    {
       "path": "/recamera_warranty",
       "image": "https://files.seeedstudio.com/wiki/reCamera/001.webp",
       "title": "reCamera Warranty",
@@ -43,44 +15,72 @@ const docList = [
   ],
   [
     {
-      "path": "/recamera_flash_os",
-      "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
-      "title": "System update & upgrade",
-      "date": "11/6/2024"
-    },
-    {
       "path": "/xiao_mg24_pin_multiplexing",
       "image": "https://files.seeedstudio.com/wiki/XIAO_MG24/Pin/top.webp",
       "title": "Pin Multiplexing with Seeed Studio XIAO MG24",
       "date": "11/6/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/watcher_software_framework",
       "image": "https://files.seeedstudio.com/wiki/watcher_software_framework/architecture_1.webp",
       "title": "Software Architecture",
       "date": "11/5/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/watcher_function_module_development_guide",
       "image": "https://files.seeedstudio.com/wiki/watcher_software_framework/watcher_function_module.webp",
       "title": "Watcher Function Module Development Guide",
       "date": "11/5/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/build_watcher_development_environment",
       "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/64.webp",
       "title": "Build the Watcher Development Environment",
       "date": "11/5/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/watcher_ui_integration_guide",
       "image": "https://files.seeedstudio.com/wiki/watcher_software_framework/ui_framework.webp",
       "title": "UI Integration Guide",
       "date": "11/5/2024"
+    },
+    {
+      "path": "/xiao_rp2350_arduino",
+      "image": "https://files.seeedstudio.com/wiki/XIAO-RP2350/img/2-102010550_XIAO_RP2350-45font_1.webp",
+      "title": "Seeed Studio XIAO RP2350 with Arduino",
+      "date": "2024-10-30T01:39:16.136Z"
+    }
+  ],
+  [
+    {
+      "path": "/SenseCAP_Indicator_Single_Channel_Gateway",
+      "image": "https://files.seeedstudio.com/wiki/SenseCAP/SenseCAP_Indicator/61.webp",
+      "title": "Single Channel LoRaWAN Gateway - SenseCAP Indicator",
+      "date": "10/30/2024"
+    },
+    {
+      "path": "/sensecap_indicator_meshtastic",
+      "image": "https://files.seeedstudio.com/wiki/SenseCAP/Meshtastic/indicator-page_1.webp",
+      "title": "Get Started with Indicator for Meshtastic",
+      "date": "10/29/2024"
+    }
+  ],
+  [
+    {
+      "path": "/recomputer_r1000_thingsboard_dashboard",
+      "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/tb/dashboard/thingsboard_1.webp",
+      "title": "Create Dynamic IoT Dashboards with ThingsBoard and reComputer R1000",
+      "date": "10/25/2024"
+    },
+    {
+      "path": "/recomputer_r1000_flash_OS",
+      "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/recomputer_r_images/01_1.webp",
+      "title": "reComputer R1000 Raspbian OS to eMMC",
+      "date": "10/25/2024"
     }
   ]
 ];
