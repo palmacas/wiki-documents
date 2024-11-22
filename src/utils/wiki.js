0c3428536eb0ca0591702066e2d5d6054a17d2bb
Jessie219-web <112080869+Jessie219-web@users.noreply.github.com>
2024-11-22 10:32:52 +0800

    update:connect to Helium via ChirpStack

--- src/utils/wiki.js
+++ src/utils/wiki.js
@@ -1,85 +1,85 @@
 const docList = [
   [
     {
+      "path": "/SenseCAP_T1000_tracker_Helium",
+      "image": "https://files.seeedstudio.com/wiki/wiki-platform/S-tempor.png",
+      "title": "Connect to Helium",
+      "date": "11/22/2024"
+    },
+    {
       "path": "/recomputer_r1000_use_rs485_modbus_rtu",
       "image": "https://files.seeedstudio.com/wiki/reComputer-R1000/recomputer_r_images/r1000_connection_1.webp",
       "title": "How to use rs485 and modbus with reComputer R1000",
       "date": "11/14/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/xiao_ra4m1_clock",
       "image": "https://files.seeedstudio.com/wiki/RA4M1_Application/top.webp",
       "title": "Hollow Clock Powered by Seeed Studio XIAO RA4M1",
       "date": "11/12/2024"
-    }
-  ],
-  [
+    },
     {
-      "path": "/recamera_get_start",
+      "path": "/recamera_getting_started",
       "image": "https://files.seeedstudio.com/wiki/reCamera/recamera_banner.webp",
       "title": "Getting Started with reCamera",
       "date": "11/11/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/reCamera_hardware_interface",
       "image": "https://files.seeedstudio.com/wiki/reCamera/image.webp",
       "title": "Hardware Interface of reCamera",
       "date": "11/11/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/iot_botton_connect_to_esphome",
       "image": "https://files.seeedstudio.com/wiki/IoT_Botton_ESPHOME/button_esphome/button_buzzer.jpg",
       "title": "Iot Button Connect To ESPHome",
       "date": "11/11/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/xiao_mg24_matter",
       "image": "https://files.seeedstudio.com/wiki/XIAO_MG24/Getting_Start/top.jpg",
       "title": "Seeed Studio XIAO MG24 Example - Matter",
       "date": "11/07/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/recamera_flash_os",
       "image": "https://files.seeedstudio.com/wiki/reCamera/001.webp",
       "title": "reCamera System update & upgrade",
       "date": "11/6/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/xiao_mg24_pin_multiplexing",
       "image": "https://files.seeedstudio.com/wiki/XIAO_MG24/Pin/2.webp",
       "title": "Pin Multiplexing with Seeed Studio XIAO MG24",
       "date": "11/6/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/watcher_software_framework",
       "image": "https://files.seeedstudio.com/wiki/watcher_software_framework/architecture_1.webp",
       "title": "Watcher Software Architecture",
       "date": "11/5/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/watcher_function_module_development_guide",
       "image": "https://files.seeedstudio.com/wiki/watcher_software_framework/watcher_function_module.webp",
       "title": "Watcher Function Module Development Guide",
       "date": "11/5/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/build_watcher_development_environment",
       "image": "https://files.seeedstudio.com/wiki/watcher_getting_started/64.webp",
       "title": "Build the Watcher Development Environment",
-      "date": "11/5/2024"
-    },
-    {
-      "path": "/watcher_ui_integration_guide",
-      "image": "https://files.seeedstudio.com/wiki/watcher_software_framework/ui_framework.webp",
-      "title": "Watcher UI Integration Guide",
       "date": "11/5/2024"
     }
   ]
