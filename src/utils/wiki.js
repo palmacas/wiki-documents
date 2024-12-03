319970dab8a75d1cd522edc76cfb1a3c700a02b6
nfs0619 <1447996181@qq.com>
2024-12-03 18:27:10 +0800

    fix wiki.js

--- src/utils/wiki.js
+++ src/utils/wiki.js
@@ -1,86 +1,86 @@
 const docList = [
   [
     {
-      "path": "/sensecraft_ai_pretrained_models_for_grove_visionai_v2",
-      "image": "https://files.seeedstudio.com/wiki/SenseCraft_AI/img2/23.png",
-      "title": "Using a model for Grove Vision AI V2",
-      "date": "12/02/2024"
-    },
-    {
-      "path": "/sensecraft_ai_pretrained_models_for_xiao",
-      "image": "https://files.seeedstudio.com/wiki/SenseCraft_AI/img2/14.png",
-      "title": "Using a model for XIAO ESP32S3 Sense",
-      "date": "12/02/2024"
-    }
-  ],
-  [
-    {
-      "path": "/relay_add_on_module_for_xiao",
-      "image": "https://files.seeedstudio.com/wiki/XIAO/Gadgets/relay_module_for_xiao/relay_module.webp",
-      "title": "Relay Add On Module For XIAO",
-      "date": "12/02/2024"
-    },
-    {
       "path": "/tinyml_course_Image_classification_project",
       "image": "https://files.seeedstudio.com/wiki/SeeedStudio-XIAO-ESP32S3/img/image_classification.webp",
       "title": "XIAO ESP32S3-Sense Image Classification",
       "date": "11/29/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/XIAO_BLE",
       "image": "https://files.seeedstudio.com/wiki/XIAO-BLE/102010469_Front-14.jpg",
       "title": "Getting Started with Seeed Studio XIAO nRF52840 Series",
       "date": "2024-11-28T03:15:24.000Z"
-    },
+    }
+  ],
+  [
     {
       "path": "/sensecraft_ai_overview",
       "image": "https://files.seeedstudio.com/wiki/SenseCraft_AI/img2/1.webp",
       "title": "SenseCraft AI Overview",
       "date": "11/28/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/2_channel_wifi_ac_relay",
       "image": "https://files.seeedstudio.com/wiki/XIAO/Gadgets/2_channel_ac_relay/3-thumbnail.webp",
       "title": "2-Channel Wi-Fi AC Relay Module In Home Assistant",
       "date": "2024-11-27T03:21:36.491Z"
-    },
+    }
+  ],
+  [
     {
       "path": "/sensecraft_ai_Training_Classification",
       "image": "https://files.seeedstudio.com/wiki/SenseCraft_AI/img3/main_page.webp",
       "title": "Type of training - Classification",
       "date": "11/27/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "/sensecraft_ai_Training_Object_Detection",
       "image": "https://files.seeedstudio.com/wiki/SenseCraft_AI/img3/object%20detection/1.9.webp",
       "title": "Type of training - Object Detection",
       "date": "11/27/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/xiao_esp32c6_zigbee_arduino",
       "image": "https://files.seeedstudio.com/wiki/xiaoc6_zigbee/3.webp",
       "title": "XIAO ESP32C6 Zigbee Quick Start Guide (Arduino)",
       "date": "11/25/2024"
-    }
-  ],
-  [
+    },
     {
       "path": "Sensor/SenseCAP/SenseCAP_LoRaWAN_Sensor/SenseCAP_S210X_Series/tutorial/How-to-Connect-SenseCAP-S210X-to-The-Things-Network",
       "image": "https://files.seeedstudio.com/wiki/SenseCAPS210X/TTN_Network/003.webp",
       "title": "Connect to The Things Network",
       "date": "11/25/2024"
-    },
+    }
+  ],
+  [
     {
       "path": "/getting-started-xiao-rp2350",
       "image": "https://files.seeedstudio.com/wiki/XIAO-RP2350/img/RP2350-thumbnail.webp",
       "title": "Getting Started with Seeed Studio XIAO RP2350(MicroPython)",
       "date": "2024-11-22T05:59:15.764Z"
+    },
+    {
+      "path": "/SenseCAP_T1000_tracker_Helium",
+      "image": "https://files.seeedstudio.com/wiki/SenseCAP/Tracker/tracker.webp",
+      "title": "Connecting SenseCAP T1000 to Helium",
+      "date": "11/22/2024"
+    }
+  ],
+  [
+    {
+      "path": "/benchmark_of_multistream_inference_on_raspberrypi5_with_hailo8",
+      "image": "https://files.seeedstudio.com/wiki/multistream_benchmark_hailo8/raspberry_pi_ai_hat.webp",
+      "title": "Benchmark of Multistream Inference on Raspberrypi 5 with Hailo8",
+      "date": "11/21/2024"
+    },
+    {
+      "path": "/XIAO-RS485-Expansion-Board",
+      "image": "https://files.seeedstudio.com/wiki/rs485_ExpansionBoard/top.webp",
+      "title": "Getting Started with Seeed Studio XIAO-RS485-Expansion-Board",
+      "date": "11/20/2024"
     }
   ]
 ];
