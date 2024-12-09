aa1421dd2d5775cc0dd3d5f1f21760c3a8ccd943
nfs0619 <1447996181@qq.com>
2024-12-09 14:22:41 +0800

    Update weekly wiki

--- sidebars.js
+++ sidebars.js
@@ -4062,10 +4062,6 @@ const sidebars = {
           label: 'ModelAssistant',
           collapsed: true,
           collapsible: true,
-          link: {
-            type: "doc",
-            id: 'Topics/TinyML/ModelAssistant/introduction/overview',
-          },
           items: [
             {
               type: 'category',
